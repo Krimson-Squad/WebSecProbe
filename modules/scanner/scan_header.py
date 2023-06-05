01f2e212ee4259ba3dc421874be3f861d1fd8ae0
devsdenepal <gautamn7822@gmail.com>
2023-06-05 15:25:32 +0545

    add header scanner

--- modules/scanner/scan_header.py
+++ modules/scanner/scan_header.py
@@ -1,30 +1,69 @@
-import requests
-
 def scan_header(url):
+    """
+    Scans the security headers of a given URL and performs vulnerability checks.
+
+    Args:
+        url (str): The URL of the web application to scan.
+
+    Returns:
+        None
+
+    Raises:
+        ValueError: If the provided URL is invalid or inaccessible.
+
+    Example:
+        scan_header("https://example.com")
+    [Contributor: Dev. Gautam Kumar]
+    """
+    import requests
     print("++++++++++++ Scanning header ++++++++++++")
 
     response = requests.get(url)
-    # Extract and print the headers
     headers = response.headers
-    print("Response Headers:")
-    for header, value in headers.items():
-        print(f"{header}: {value}")
-    
-    # Perform further analysis or checks on the headers
-    
-    # Example: Check for specific headers
+
+    # Content Security Policy (CSP) Scanner
     if 'Content-Security-Policy' in headers:
-        pass
+        print("[+] Content-Security-Policy header found.")
+        # Perform further checks or actions related to CSP
     else:
-        print("Content-Security-Policy header not found.")
-    
+        print("\033[91m[-] Content-Security-Policy header not found.\033[0m")
+        print("Reference: [OWASP Content Security Policy](https://owasp.org/www-project-secure-headers/#content-security-policy)")
+
+    # X-XSS-Protection Scanner
     if 'X-XSS-Protection' in headers:
-        print("X-XSS-Protection header found.")
+        print("[+] X-XSS-Protection header found.")
+        # Perform further checks or actions related to XSS protection
     else:
-        print("X-XSS-Protection header not found.")
-    
-    # ... Continue checking for other headers
-    
+        print("\033[91m[-] X-XSS-Protection header not found.\033[0m")
+        print("Reference: [OWASP XSS (Cross Site Scripting) Prevention Cheat Sheet](https://owasp.org/www-community/xss-filter-evasion-cheatsheet)")
+
+    # X-Frame-Options Scanner
+    if 'X-Frame-Options' in headers:
+        print("[+] X-Frame-Options header found.")
+        # Perform further checks or actions related to clickjacking protection
+    else:
+        print("\033[91m[-] X-Frame-Options header not found.\033[0m")
+        print("Reference: [OWASP Clickjacking Defense Cheat Sheet](https://owasp.org/www-community/attacks/Clickjacking)")
+
+    # HSTS (Strict-Transport-Security) Scanner
+    if 'Strict-Transport-Security' in headers:
+        print("[+] Strict-Transport-Security header found.")
+        # Perform further checks or actions related to HSTS
+    else:
+        print("\033[91m[-] Strict-Transport-Security header not found.\033[0m")
+        print("Reference: [OWASP Transport Layer Protection Cheat Sheet](https://owasp.org/www-project-secure-headers/#http-strict-transport-security-hsts)")
+
+    # CORS (Cross-Origin Resource Sharing) Scanner
+    if 'Access-Control-Allow-Origin' in headers:
+        print("[+] CORS headers found.")
+        # Perform further checks or actions related to CORS configuration
+    else:
+        print("\033[91m[-] CORS headers not found.\033[0m")
+        print("Reference: [OWASP Cross-Origin Resource Sharing (CORS) Cheat Sheet](https://owasp.org/www-community/attacks/CORS_OriginHeaderScrutiny)")
+
+    # Members may add other headers scanners...
+    # Add more scanners for additional security headers as needed
+
     # Access specific header values
     content_type = headers.get('Content-Type')
     server = headers.get('Server')
@@ -32,5 +71,5 @@ def scan_header(url):
     
     # Perform additional tasks or analysis based on the headers
     
- # Usage
+# Usage
 # scan_header("https://example.com")
