6e65dfdb5f5ee7ffaa2c32d1c42cf1c3a699f703
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-06-06 16:54:27 +0545

    add chromedriver configuration

---
+++ modules/config/chromedriver.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+# Configure ChromeDriver
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--no-sandbox')
+# Initialize ChromeDriver
+driver = webdriver.Chrome('../../resources/chromedriver', options=chrome_options)
