605291a42337101375dabc3079d4fff179f4f817
Dev. Gautam Kumar <111997815+devsdenepal@users.noreply.github.com>
2023-06-07 09:16:48 +0545

    Update chromedriver.py

--- modules/config/chromedriver.py
+++ modules/config/chromedriver.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
 # Configure ChromeDriver
 chrome_options = webdriver.ChromeOptions()
+chrome_option.binary_location = "C:\Program Files\Google\Chrome\Application\chrome.exe"
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--no-sandbox')
 # Initialize ChromeDriver
-driver = webdriver.Chrome('../../resources/chromedriver.exe', options=chrome_options)
+driver = webdriver.Chrome(executable_path='../../resources/chromedriver.exe', options=chrome_options)
